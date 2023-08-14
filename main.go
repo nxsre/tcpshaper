9f595f5ec31d635e34aa2fd2322d228f09d2da33
nxsre <32614138+nxsre@users.noreply.github.com>
2023-08-14 17:22:17 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,47 +1,40 @@
 package main
 
 import (
-	"context"
-	"flag"
-	"github.com/gerritjvv/tcpshaper/bandwidth"
-	"github.com/rjeczalik/netxtest"
-	"log"
-	"net"
+  "context"
+  "fmt"
+  "github.com/gerritjvv/tcpshaper/bandwidth"
+  "net"
 )
 
-func tcpshaperListener(l net.Listener, limitGlobal, limitPerConn int) net.Listener {
+func main() { 
+  // Get a Listener, e.g tcp on any port
+  tcpListner, err := net.Listen("tcp", ":0")
+  if err != nil {
+   panic(err)
+  }
 
-	ctx := context.Background()
+  ctx := context.Background()
 
-	// For the test go run main.go -count 100 -limit-conn 0
-	// The test itself calculates a range as a multiple of the connection count
-	// e.g: want = 30s * t.Limit/t.Count => 30s * (26214400 / 100)
-	//  I tried to dynamically adjust the connection rate based on the existing connection count
-	//   but this did not pass the test either, because the conn count can be 1, 10, 100 etc at any moment.
-	if limitPerConn == 0 {
-		limitPerConn = limitGlobal / 100
-	}
+  // Configure rate limit to:
+  // total read+write traffic == 1mbs
+  // Each connection read+write == 2kbs
+  // The maximum that can be read or written at any one time is 2kb
+  serverRate := bandwidth.NewRateConfig(1024*1024, 2048)
+  connRate := bandwidth.NewRateConfig(2048, 2048)
 
-	serverRate := bandwidth.NewRateConfig(int64(limitGlobal), limitGlobal)
-	connRate := bandwidth.NewRateConfig(int64(limitPerConn), limitPerConn)
+  listener := bandwidth.NewListener(ctx, &bandwidth.ListenerConfig{
+    ReadServerRate:  serverRate,
+    WriteServerRate: serverRate,
+    ReadConnRate:    connRate,
+    WriteConnRate:   connRate,
+  }, tcpListner)
 
-	listener := bandwidth.NewListener(ctx, &bandwidth.ListenerConfig{
-		ReadServerRate:  serverRate,
-		WriteServerRate: serverRate,
-		ReadConnRate:    connRate,
-		WriteConnRate:   connRate,
-	}, l)
-
-	return listener
-}
-
-func main() {
-	var test netxtest.LimitListenerTest
-
-	test.RegisterFlags(flag.CommandLine)
-	flag.Parse()
-
-	if err := test.Run(tcpshaperListener); err != nil {
-		log.Fatal(err)
-	}
+  // Now use the listener
+  // e.g listener.Accept()
+  
+  err = listener.Close()
+  if err != nil {
+    fmt.Printf("error while closing listener %s", err)
+  }
 }
